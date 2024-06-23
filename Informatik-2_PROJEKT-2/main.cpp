0c91e14ce613fa01717ab99766361c88dc06199f
rungeAm <rungeandreasm@gmail.com>
2024-06-23 19:31:02 +0200

    qefqeq

--- Informatik-2_PROJEKT-2/main.cpp
+++ Informatik-2_PROJEKT-2/main.cpp
@@ -121,7 +121,7 @@ int main()
 	else
 	{
 		std::thread myThread([&]() {
-			int fatal_err = firstUser.User_bind_listen_accept(OWNIP, CONNECTIP, Port, 0, DEBUG_MODE);
+			int fatal_err = firstUser.User_bind_listen_accept(OWNIP, firstUser.IP_Store[0], Port, 0, DEBUG_MODE);
 			if (fatal_err == -1) {
 				std::cout << "Error in User_bind_listen_accept!" << std::endl;
 			
